ad551a011ac763532dae84a8196e56b31819d341
CristianEtchebest <cristian.etchebest@meta.com.br>
2020-12-08 15:43:20 -0300

    Implementação listagem de Pokemons

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -4,28 +4,25 @@
         <form action="">                      
                 <div class="form-group row ">
                     <label for="" class="col-sm-3 col-form-label" >Escolha o Pokemon:</label>
-                    <div class="col-sm-9">
-                        <!--select name="listPoke" id="listPoke" class="form-control col-sm-7" >
-                            <option v-for="todo in todos" :value='todo.count' :key="todo.name" >{{ todo.name }}</option>
-                        </select-->
+                    <div class="col-sm-5">
+                        <select name="listPoke" id="listPoke" class="form-control col-sm-7" v-model="pokeList" >
+                            <option v-for="pokemon in pokemons" :value='pokemon.url' :key="pokemon.name" > {{ pokemon.name }}</option>
+                        </select>                      
+
                     </div>
-                    
+                    <div class="col-sm-4">
+                        <button class="btn btn-primary" type="submit"   v-on:click.prevent="addDados">Ver daddos</button>
+                    </div>     
+                         
                 </div>                
         </form>
-        <hr>
-        <table>
-            <tbody>
-                <p>{{todos}}</p>
-            <tr v-for="todo in todos" :key="todo">
-            <td>{{todo.name}}</td>
-            </tr>
-
-            </tbody>
-            </table>    
-            
+        <hr> 
+           
         <div class="content ">
-            <h3>Dados do Pokemon</h3>
-            <p>Nome: </p>
+            <h3 >Dados do Pokemon</h3>
+            <p>Nome: {{pokeDados.name}}</p>
+            <p>Altura: {{pokeDados.height}}</p>
+            <p>Peso: {{pokeDados.weight}}</p> 
             <p>Habilidade: </p>
             <p>Velocidade: </p>
             <p>Defesa: </p>
@@ -34,7 +31,7 @@
             <p>Total: </p>
             <h3>Evoluções</h3>
             <p>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" /> >> 
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"  alt="" /> >> 
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" alt="" /> >>
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" alt="" /> 
             </p>
@@ -43,30 +40,35 @@
     </div>
 </template> 
 
+
 <script>
 import axios from 'axios'
 export default { 
     data(){
         return {
-         /*name: 'Lista de Pokemons',
-         poke: null,         
-         litasPokemons: [],*/
-         todos:[]
-        }
+            namei: 'Lista de Pokemons',    
+            pokemons:null,
+            pokeDados:""        
+        } 
     },
-    mounted(){
-        //this.listarDados();      
-        this.getTodos();
+    mounted(){   
+        this.getTodos();   
     },
 
     methods: {
+        // Pega a URL para listar os pokemons
         getTodos(){
             axios
-            .get('http://pokeapi.co/api/v2/pokemon/')
-            .then( Response => {this.todos = Response.data })
-            .catch(e => console.log(e))
-        }        
-    }    
+            .get("http://localhost:8000/api/listar/")
+            .then( response => (this.pokemons = response.data))       
+        },
+
+        //Exibe o array na variavel pokeDados para ser exibida no campos de informações do pokemon
+
+        addDados(){
+            axios.get(this.pokeList).then(response => this.pokeDados = response.data) 
+        }
+    }
 
 }
 </script>
